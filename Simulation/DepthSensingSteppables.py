20e1ae2c2cb20e8a3989029d87b3b12c9ad101bc
dbarraav <dbarraa98@gmail.com>
2024-08-14 10:57:25 -0500

    Add comments to code

--- Simulation/DepthSensingSteppables.py
+++ Simulation/DepthSensingSteppables.py
@@ -24,17 +24,20 @@ def FindAngle(pix, pop_com): #BOTH NP ARRAYS
     return angle_raw
 
 def FindAlpha(angle360, cell_angle_list, alpha_list):
-
-    temp = np.absolute(cell_angle_list-angle360)
-    cell_location = np.argmin(temp)
-    alpha = alpha_list[cell_location]   
-    
+    # Get alpha from minimum absolute difference
+    temp = np.absolute(cell_angle_list-angle360) # Get list of absolute differences
+    cell_location = np.argmin(temp) # Get minimum of absolute differences
+    alpha = alpha_list[cell_location]  # Get alpha at minimum absolute difference
     return alpha
 
 def GetProbOfUsingProb(max_alpha):
+    # Get minimum between 1 and f(alpha)
     return min(1, (max_alpha**2)/2)
 
-def CalculateFeedback(max_alpha):            
+def CalculateFeedback(max_alpha):      
+    # Get feedback based on maximum alpha and and alpha threshold
+    # Dependent on whether m is equal to 110 or not
+    # Also dependent on whether laser ablation variable is 1 or not      
     feedback= 0.0 #soft but thresh crossed
     if max_alpha > max_alpha_thresh and m== 110:
         feedback= 1
@@ -58,11 +61,14 @@ class UpdatePhi(SteppableBasePy):
         
 
     def step(self, mcs):
+
+        # 
         count_boundary= 0
-        pop_com = np.array([0.0,0.0])
-        count= 0
+        pop_com = np.array([0.0,0.0]) # COM of all cells
+        count = 0
         for cell in self.cell_list_by_type(self.CELL):
-            cell.dict['IsBoundary'] = 0
+            # ['IsBoundary'] is to determine if cell is at periphery of collective
+            cell.dict['IsBoundary'] = 0 
             pop_com += np.array([cell.xCOM,cell.yCOM])
             for neighbor, common_surface_area in self.get_cell_neighbor_data_list(cell):
                 if not neighbor:
@@ -71,45 +77,61 @@ class UpdatePhi(SteppableBasePy):
                     break
             count+= 1
 
-        pop_com /= count
+        pop_com /= count # COM of all cells
         self.shared_steppable_vars['pop_com'] = pop_com
 
+        # Initialize fields for phi, rm_abi, and alpha
         field_phi = self.field.phi_field
         field_rm_abi = self.field.rm_abi
         field_alpha = self.field.alpha
+        field_IsBoundary = self.field.IsBoundary
+
         field_phi[:,:,:] = 0.0
         field_rm_abi[:,:,:] = 0.0
-
-        field_IsBoundary = self.field.IsBoundary
         field_IsBoundary[:,:,:] = 0.0
 
+        # Get chosen_angle and make it between 0 and 2*pi radians
         chosen_angle= self.shared_steppable_vars['chosen_angle']
         if chosen_angle >= 2*3.14:
             chosen_angle= chosen_angle - 2*3.14
-            
+        
+
+        # Get lists of cell angles and alphas
         cell_angle_list = []
         alpha_list= []
-        angle_cell_dict= {}
+        angle_cell_dict = {}
         count_active= 0
         max_alpha= self.shared_steppable_vars['max_alpha']
         # print("max_alpha",max_alpha)
         feedback= CalculateFeedback(max_alpha)
+
         for cell in self.cell_list_by_type(self.CELL):
             phi = phi0
             rm_abi = 0
             # cell.dict['phi'] = phi
             cell.dict['rm_abi'] = rm_abi
+            
+            # Check cell only if on periphery of the collective
             IsBoundary = cell.dict['IsBoundary']
             if IsBoundary == 1:
-                cell_com = np.array([cell.xCOM, cell.yCOM])
-                cell_angle = FindAngle(cell_com, pop_com)
-                cell_angle_list.append(cell_angle)
-                alpha_cell= field_alpha[cell.xCOM, cell.yCOM, cell.zCOM]
-                alpha_list.append(alpha_cell)
-                angle_cell_dict[cell_angle]= alpha_cell
-                cell_unit_vec= np.array([np.cos(cell_angle),np.sin(cell_angle)]) 
+                cell_com = np.array([cell.xCOM, cell.yCOM]) # Get cell COM
+                cell_angle = FindAngle(cell_com, pop_com) # Get angle between COM_cell and COM_collective
+                alpha_cell= field_alpha[cell.xCOM, cell.yCOM, cell.zCOM] # Get alpha only at the cell's COM
+                angle_cell_dict[cell_angle]= alpha_cell # Make dictionary - {angle : alpha}
+                
+                cell_angle_list.append(cell_angle) # Append angle to list of angles
+                alpha_list.append(alpha_cell) # Append cell's alpha to list of alphas
+                
+                # Get unit vector from boundary cell's angle
+                cell_unit_vec= np.array([np.cos(cell_angle),np.sin(cell_angle)])
+
+                # Get vector for whole cell's migration direction
                 chosen_angle_unit_vec= np.array([np.cos(chosen_angle), np.sin(chosen_angle)])
+
                 # if (np.absolute(cell_angle-chosen_angle) < half_cone*3.14/180 ) or (np.absolute(2*3.14-(cell_angle-chosen_angle)) < half_cone*3.14/180 ):
+
+                # Dot product of unit vectors give cos(theta) in radians
+                # Compare result to cos(half_cone) in radians
                 if np.dot(cell_unit_vec, chosen_angle_unit_vec) >= np.cos(half_cone*(3.14/180) ):
                     rm_abi= 1
                     # phi= (mcs >= 50)*(m_current == 110)*(depth_sensed) + phi0
@@ -121,6 +143,7 @@ class UpdatePhi(SteppableBasePy):
                 cell.dict['rm_abi'] = rm_abi
                 cell.dict['phi'] = phi
 
+            # For each pixel of cell, assign phi, rm_abi and IsBoundary
             pixel_list = self.get_cell_pixel_list(cell)
             for pixel_tracker_data in pixel_list:
                 px = pixel_tracker_data.pixel
@@ -148,17 +171,7 @@ class DoRemodeling(SteppableBasePy):
     def __init__(self, frequency=1):
         SteppableBasePy.__init__(self,frequency)
     def start(self):
-        
-        self.plot_win1 = self.add_new_plot_window(
-            title='InstSpeed',
-            x_axis_title='MonteCarlo Step (MCS)',
-            y_axis_title='Speed',
-            x_scale_type='linear',
-            y_scale_type='linear',
-            grid=True # only in 3.7.6 or higher
-        )
-        self.plot_win1.add_plot("InstSpeed", style='Dots', color='red', size=5)                
-
+        # Determine the COM of all cells at start of simulation
         pop_com = np.array([0.0,0.0])
         count= 0
         for cell in self.cell_list_by_type(self.CELL):
@@ -176,10 +189,11 @@ class DoRemodeling(SteppableBasePy):
         cell_angle_list = self.shared_steppable_vars['cell_angle_list']
         alpha_list = self.shared_steppable_vars['alpha_list']
 
+        # Get pixels for medium/ECM
         for x, y, z in self.every_pixel():
             if not field_cell[x,y,z]: # ONLY ECM PIXELS
                 pix = np.array([x,y])
-                angle = FindAngle(pix,pop_com)
+                angle = FindAngle(pix,pop_com) # Find angle between ECM pixel and COM of cell collective
                 alpha_propagate = FindAlpha(angle, cell_angle_list, alpha_list)
                 # cluster_num = FindCluster(pix, pop_com)
                 # alpha_LE = cluster_alpha[cluster_num]
@@ -194,35 +208,30 @@ class DoRemodeling(SteppableBasePy):
         if mcs%100 == 0 and mcs !=0 :#YOU DONT WANT ANYTHING AT ZERO AS ITS TRIVIAL
             delta_d= pop_com - self.shared_steppable_vars['old_pop_com']
             self.shared_steppable_vars['old_pop_com'] = pop_com
-            self.plot_win1.add_data_point('InstSpeed', mcs, self.vector_norm(delta_d)/100)
 
             with open(output_path, 'a') as fout:
                 fout.write('{} {} \n'.format(pop_com[0],pop_com[1]))
 
 
     def finish(self):
-        if self.output_dir is not None:
-            png_output_path = Path(self.output_dir).joinpath("Plot_inst_speed" + ".png")
-            self.plot_win1.save_plot_as_png(png_output_path)
-            csv_output_path = Path(self.output_dir).joinpath("data_inst_speed"+ ".csv")
-            self.plot_win1.save_plot_as_data(csv_output_path, CSV_FORMAT)            
+        pass
 
-class FindECMpolarity(SteppableBasePy): #FIND THE NET DIRECTION OF ECM POLARITY, 
+class FindECMpolarity(SteppableBasePy): 
+#FIND THE NET DIRECTION OF ECM POLARITY, 
 # DO THE RANDOM DISTRIBUTIONS AND PICK A NEW FRONT/BACK THETA, TO BE FED INTO UPDATE_RM
     def __init__(self, frequency=200):
         SteppableBasePy.__init__(self,frequency)
 
-    def step(self, mcs):       
+    def step(self, mcs):     
+
         cell_angle_sort= np.array(self.shared_steppable_vars['cell_angle_sort'])
-        # print("type_1",type(cell_angle_sort) )
-        # print("type_2", type(cell_angle_sort[0]))
-        # print("type_3", type(cell_angle_sort[0][0]))
-        num_boundary= len(cell_angle_sort)
-        # print("cell_angle_sort", cell_angle_sort)
-        # print("num_b", num_boundary)
+        num_boundary= len(cell_angle_sort) # get length of list of cell_angle_sort
+
         angle_avg_alpha_avg= []
-        
+        # For each cluster in number of boundaries (num_boundary)
         for cluster_num in range(num_boundary):
+            # cell_angle_sort has cluster_num in rows of array
+            # cell_angle_sort has angle as first column of array
             start_angle= cell_angle_sort[cluster_num,0]
             final_angle= start_angle
             sum_angles= 0
@@ -230,26 +239,29 @@ class FindECMpolarity(SteppableBasePy): #FIND THE NET DIRECTION OF ECM POLARITY,
             next_cluster= cluster_num
             flag_add_pi= 0
             counter=0
-            
+
+            # Check if difference between angles is less than twice half_cone (in radians)
             while final_angle-start_angle < half_cone*2*(3.14/180): 
                 sum_angles+= final_angle
-                sum_alphas+= (cell_angle_sort[next_cluster - flag_add_pi*(num_boundary),1])**3 #make sure its not part of the wrap-around
+                #make sure its not part of the wrap-around
+                sum_alphas+= (cell_angle_sort[next_cluster - flag_add_pi*(num_boundary),1])**3 
                 #Making probability proportional to sum of cube of alphas, rather than cube of sum of alphas
                 if next_cluster== (num_boundary - 1): # i.e. IT IS AT THE LAST POINT
                     flag_add_pi= 1 # Add 2pi to the initial angles when wrapping around
                 next_cluster+= 1  
                 final_angle= (2*3.14)*flag_add_pi + cell_angle_sort[next_cluster - flag_add_pi*(num_boundary),0] # WRAP ARONUD THE LIST
                 counter+= 1
-                
-            angle_avg_alpha_avg.append( ((sum_angles)/counter, (sum_alphas)/counter) )
             
+            # Append tuple of average angle and average alpha
+            angle_avg_alpha_avg.append( ((sum_angles)/counter, (sum_alphas)/counter) )
+
+        # Make numpy array from list of tuples of average angle and average alpha    
         angle_avg_alpha_avg= np.array(angle_avg_alpha_avg)
-        # print("ang_alp", angle_avg_alpha_avg)
         angles= angle_avg_alpha_avg[:,0]
         alphas= angle_avg_alpha_avg[:,1]
-        # print("alphas", alphas )
-        # print("angles", angles)
+
         direction_index= 0
+        # cell_angle_sort has angle as first column of array
         max_alpha= max(np.array(cell_angle_sort)[:,1])
         prob_of_using_prob= GetProbOfUsingProb(max_alpha) 
         
@@ -260,18 +272,17 @@ class FindECMpolarity(SteppableBasePy): #FIND THE NET DIRECTION OF ECM POLARITY,
             alphas_p= alphas/np.sum(alphas)
             # print(alphas_p)
             direction_index= np.random.choice(int(len(alphas)),1,p= alphas_p)
-            # print("1",direction_index)
+            
         else:
+            # Pick random number from numbers between 0 and length of alphas list
             direction_index= np.random.choice(int(len(alphas)),1)
-            # print("2", direction_index)
+
+        # chosen_angle is picked from average angles list, witch each angle corresponding to specific alpha
         chosen_angle= angles[direction_index]
         self.shared_steppable_vars['chosen_angle']= float(chosen_angle)
         # print("chosen_direcion",180/3.14*float(chosen_angle))
 
-               
-                
-                
-                
+
             # extra_angles_modified= []
             # end_loc= start_loc+num_boundary//2
             # if end_loc > num_boundary:#NO MINUS ONE BECASUE WE ARE USING SLICES, THERE AUTOMATIC-1 HAPPENS AT THE LAST INDEX
@@ -316,9 +327,11 @@ class FindECMpolarity(SteppableBasePy): #FIND THE NET DIRECTION OF ECM POLARITY,
         # self.shared_steppable_vars['chosen_angle']= float(chosen_angle)
         # # print("chosen_direcion",float(chosen_angle))
 
-            
-class SetCellForces(SteppableBasePy):
 
+
+# Set forces for cells using "chosen_angle"
+# Magnitude of protrusive force is equal to 10*phi
+class SetCellForces(SteppableBasePy):
     def __init__(self, frequency=1):
 
         SteppableBasePy.__init__(self,frequency)
@@ -329,16 +342,11 @@ class SetCellForces(SteppableBasePy):
         """
 
     def step(self, mcs):
-
-
         chosen_angle = self.shared_steppable_vars['chosen_angle']
         for cell in self.cell_list_by_type(self.CELL):
             cell.lambdaVecX = -10*cell.dict['phi']*np.cos(chosen_angle)  # force component pointing along X axis - towards positive X's
             cell.lambdaVecY = -10*cell.dict['phi']*np.sin(chosen_angle)  # force component pointing along Y axis - towards negative Y's
             cell.lambdaVecZ = 0.0  # force component pointing along Z axis
-
-            
-            
 
     def finish(self):
         """
